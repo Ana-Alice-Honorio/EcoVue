1b0bc1dde90f5e6d778e4ef030d9b5cbcb2b4eed
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2024-03-10 20:40:31 -0300

    estlização cards e listagem dos cards

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,10 +3,10 @@
     <img :src="product.thumbnail" :alt="product.title" class="product-image" />
     <div class="product-details">
       <h3>{{ product.title }}</h3>
-      <p>{{ product.description }}</p>
-      <p>Preço: R$ {{ product.price }}</p>
+      <!-- <p>{{ product.description }}</p> -->
+      <p>R$ {{ product.price }}</p>
     </div>
-  </div>
+ </div>
 </template>
 
 <script>
@@ -16,4 +16,66 @@ export default {
   }
 };
 </script>
+
+<style scoped lang="scss">
+.product-card {
+  position: relative;
+  width: 235px;
+  height: 355px;
+  font-family: "Roboto", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #eee;
+  border-radius: 4px;
+  overflow: hidden;
+  cursor: pointer;
+
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    inset: 0;
+    object-fit: cover;
+  }
+  
+  .product-details {
+    padding: 16px;
+    width: 100%;
+    height: 157px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    gap: 8px;
+    background: linear-gradient(to bottom,#00000000, #00000080, #00000080, #000000);
+
+  h3 {
+    font-size: 14px;
+    font-weight: 500;
+    }
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .product-card {
+    width: 183px;
+    height: 281px;
+
+  .product-details h3 {
+    font-size: 14px;
+    font-weight: 700;
+  }
+ }
+}
+
+@media only screen and (width < 414px) {
+  .movie-card {
+    width: 235px;
+    height: 355px;
+  }
+}
+</style>
 
--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <h1>Produtos</h1>
     <div class="product-list">
       <ProductCard v-for="product in products" :key="product.id" :product="product" />
     </div>
@@ -26,4 +25,30 @@ export default {
 };
 </script>
 
+<style scoped lang="scss">
+.home {
+  padding-top: 100px;
+}
 
+.product-list {
+  margin: 0 auto;
+  padding: 24px;
+  max-width: 1322px;
+  min-height: 355px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 24px;
+  background-color: var(--mauve-a3);
+  backdrop-filter: blur(2px);
+  border-radius: 4px;
+}
+
+@media only screen and (max-width: 768px) {
+  .product-list {
+    padding: 16px;
+    gap: 16px;
+    background-color: var(--mauve-3);
+  }
+}
+</style>

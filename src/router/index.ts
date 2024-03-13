5b2af37b7a2e91dd0d82e05d44c7cc94dacebfe5
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2024-03-12 21:10:48 -0300

    página notfound

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ProductList from '@/views/ProductList.vue'
 import ProductsDetails from '@/views/ProductsDetails.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'ProductDetails',
       component: ProductsDetails
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound
+    }
   ]
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="not-found">
+        <div class="body">
+          <img src="../../public/notfound.png"/>
+          <p>A página que você está procurando não existe.</p>
+          <router-link to="/" class="back-link">Voltar para a página inicial</router-link>
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFound'
+  };
+  </script>
+  
+  <style scoped lang="scss">
+.not-found {
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+   height: 92vh;
+   background-color: var(--mauve-a3);
+
+  .back-link {
+    font-family: Arial, sans-serif;
+    color: var(--green-9); 
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline; 
+    }
+  }
+
+  .body {
+    flex-grow: 1;
+    text-align: center;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+}
+
+  </style>
+  
+  
